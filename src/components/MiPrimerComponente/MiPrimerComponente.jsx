8a684470463cd025995c6a181e602b4f8a9b26a8
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2024-02-07 09:52:48 -0600

    Little changes

--- src/components/MiPrimerComponente/MiPrimerComponente.jsx
+++ src/components/MiPrimerComponente/MiPrimerComponente.jsx
@@ -3,10 +3,18 @@ import React, { useState } from 'react'
 const MiPrimerComponente = ({nombre, saludo}) => {
     //Lógica de componente
     const [contador, setContador] = useState(0)
+    const add=() =>{
+        setContador(contador + 1)
+    }
+    const remove=() =>{
+        setContador(contador -1)
+    }
   return (
     //HTML que retorna
-    <div className='w-48 aspect-square bg-purple-600 text-white
-    text-5xl'>
+    <div 
+        onClick={add}
+        className='w-48 aspect-square bg-purple-600 text-white
+        text-5xl'>
         {saludo} {nombre}, {contador}</div>
   )
 }
